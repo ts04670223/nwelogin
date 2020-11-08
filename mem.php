06c6071a03e9ffd88e5245d4e5fc999f217d7188
mackliu <macklun@gmail.comm>
2020-11-08 18:22:19 +0800

    簡易會員註冊登入管理系統樣版

---
+++ mem.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>一般會員中心</title>
+</head>
+<body>
+<h1>一般會員中心</h1> 
+親愛的  你好，歡迎你
+</body>
+</html>
---
+++ vip.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>白金會員中心</title>
+</head>
+<body>
+<h1>白金會員中心</h1>
+尊爵的  你好，歡迎你
+</body>
+</html>

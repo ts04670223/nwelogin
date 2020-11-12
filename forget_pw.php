cae1f0b01bd17917a541656e45966385ca184e3e
johy <ts04670223@gmail.com>
2020-11-12 11:51:34 +0800

    增加忘記密碼查詢

--- forget_pw.php
+++ forget_pw.php
@@ -1,11 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>忘記密碼</title>
 </head>
+
 <body>
-    
+
+    <?php
+    if (isset($_POST['email'])) {
+        $dsn = "mysql:host=localhost;dbname=member;charset=utf8";
+        $pdo = new PDO($dsn,'root','');
+        $sql = "select * from login where email='{$_POST['email']}'";
+        $chk = $pdo->query($sql)->fetch();
+        // echo "<pre>";
+        // print_r($chk);
+        // echo "</pre>";
+        if (!empty($chk)) {
+            $res = $chk['pw'];
+        } else {
+            $res ="查無此人";
+        }
+    }
+
+
+
+
+
+    ?>
+    <form action="?" method="post">
+        <input type="text" name="email">
+        <input type="submit" value="查詢">
+    </form>
+    <span><?php
+    if(isset($res)){echo $res;};
+    ?></span>
 </body>
+
 </html>
